999bdb6adf35ac478ef45b2ae4dfabd4c07388e3
byron korir <byron.korir@student.moringaschool.com>
2024-01-02 21:43:25 +0300

    complete assignment

---
+++ server/migrations/versions/c356eecc283f_create_tables_owners_pets.py
@@ -0,0 +1,24 @@
+"""Create tables owners, pets
+
+Revision ID: c356eecc283f
+Revises: 
+Create Date: 2024-01-02 21:38:01.599084
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c356eecc283f'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
